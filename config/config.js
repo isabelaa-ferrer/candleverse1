bbe09c526c90f9e64d4f15e89782bc0dca7f1f86
isabelaa-ferrer <contatoisabelaferrer@gmail.com>
2025-10-28 16:31:24 -0300

    Delete config directory

--- config/config.js
+++
@@ -1,9 +0,0 @@
-// /config/config.js
-export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID"
-};
